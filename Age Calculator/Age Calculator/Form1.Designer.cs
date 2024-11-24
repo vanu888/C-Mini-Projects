c5801c9a492aa9b5931af96fe0aed158f027c0ab
vanu888 <avihanga888@gmail.com>
2024-11-24 07:59:09 +0530

    Upload projects

---
+++ Age Calculator/Age Calculator/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Age_Calculator
+{
+    partial class FrmAgeCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblThisYear = new System.Windows.Forms.Label();
+            this.lblBirthYear = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtThisYear = new System.Windows.Forms.TextBox();
+            this.txtBirthYear = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(55, 28);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(171, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Age Calculator";
+            // 
+            // lblThisYear
+            // 
+            this.lblThisYear.AutoSize = true;
+            this.lblThisYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThisYear.Location = new System.Drawing.Point(28, 82);
+            this.lblThisYear.Name = "lblThisYear";
+            this.lblThisYear.Size = new System.Drawing.Size(84, 20);
+            this.lblThisYear.TabIndex = 1;
+            this.lblThisYear.Text = "This Year :";
+            // 
+            // lblBirthYear
+            // 
+            this.lblBirthYear.AutoSize = true;
+            this.lblBirthYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBirthYear.Location = new System.Drawing.Point(28, 132);
+            this.lblBirthYear.Name = "lblBirthYear";
+            this.lblBirthYear.Size = new System.Drawing.Size(88, 20);
+            this.lblBirthYear.TabIndex = 2;
+            this.lblBirthYear.Text = "Birth Year :";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(74, 179);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(151, 179);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtThisYear
+            // 
+            this.txtThisYear.Location = new System.Drawing.Point(126, 82);
+            this.txtThisYear.Name = "txtThisYear";
+            this.txtThisYear.Size = new System.Drawing.Size(100, 20);
+            this.txtThisYear.TabIndex = 5;
+            // 
+            // txtBirthYear
+            // 
+            this.txtBirthYear.Location = new System.Drawing.Point(126, 132);
+            this.txtBirthYear.Name = "txtBirthYear";
+            this.txtBirthYear.Size = new System.Drawing.Size(100, 20);
+            this.txtBirthYear.TabIndex = 6;
+            // 
+            // FrmAgeCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtBirthYear);
+            this.Controls.Add(this.txtThisYear);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblBirthYear);
+            this.Controls.Add(this.lblThisYear);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "FrmAgeCalculator";
+            this.Text = "Age Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblThisYear;
+        private System.Windows.Forms.Label lblBirthYear;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtThisYear;
+        private System.Windows.Forms.TextBox txtBirthYear;
+    }
+}
+
---
+++ Age Calculator/Age Calculator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Age_Calculator
+{
+    public partial class FrmAgeCalculator : Form
+    {
+        public FrmAgeCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int This_Year, Birth_Year, Age;
+            This_Year = Convert.ToInt32(txtThisYear.Text);
+            Birth_Year = Convert.ToInt32(txtBirthYear.Text);
+            if (This_Year < Birth_Year)
+            {
+                MessageBox.Show("Invalid year!");
+            }
+            else
+            {
+                Age = This_Year - Birth_Year;
+                if ((Age < 1) || (Age > 100))
+                {
+                    MessageBox.Show("Enter real year !");
+                }
+                else
+                {
+                    MessageBox.Show("Your age is " + Age);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtThisYear.Clear();
+            txtBirthYear.Clear();
+        }
+    }
+}
