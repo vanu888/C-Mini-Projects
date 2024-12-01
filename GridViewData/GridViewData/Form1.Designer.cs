ef552c4528133d37fb56712705633600bcd522d2
vanu888 <avihanga888@gmail.com>
2024-12-01 16:50:06 +0530

    Upload projects

---
+++ GridViewData/GridViewData/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace GridViewData
+{
+    partial class FrmGridView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dtgridview1 = new System.Windows.Forms.DataGridView();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgridview1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(25, 69);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(49, 16);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(108, 273);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 34);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(108, 69);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(161, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // dtgridview1
+            // 
+            this.dtgridview1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgridview1.Location = new System.Drawing.Point(291, 69);
+            this.dtgridview1.Name = "dtgridview1";
+            this.dtgridview1.Size = new System.Drawing.Size(342, 238);
+            this.dtgridview1.TabIndex = 3;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(25, 139);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(36, 16);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "Age";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(25, 199);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(34, 16);
+            this.lblCity.TabIndex = 5;
+            this.lblCity.Text = "City";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(108, 132);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(161, 20);
+            this.txtAge.TabIndex = 6;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(108, 199);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(161, 20);
+            this.txtCity.TabIndex = 7;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(194, 273);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 34);
+            this.btnRemove.TabIndex = 8;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // FrmGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 356);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.dtgridview1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblName);
+            this.Name = "FrmGridView";
+            this.Text = "GridView";
+            this.Load += new System.EventHandler(this.FrmGridView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgridview1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DataGridView dtgridview1;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ GridViewData/GridViewData/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GridViewData
+{
+    public partial class FrmGridView : Form
+    {
+        /*This is a simple and basic datagridview project*/
+        /*Use public DataTable*/
+        public DataTable studentInfo;
+        public FrmGridView()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmGridView_Load(object sender, EventArgs e)
+        {
+            /*Create a variable for DataTable*/
+            studentInfo = new DataTable("studentInfo");
+
+            /*Add Data Columns for each title*/
+            DataColumn stu_name = new DataColumn("Name", typeof(string));
+            studentInfo.Columns.Add(stu_name);
+
+            DataColumn stu_age = new DataColumn("Age", typeof(Int32));
+            studentInfo.Columns.Add(stu_age);
+
+            DataColumn stu_city = new DataColumn("City", typeof(string));
+            studentInfo.Columns.Add(stu_city);
+
+            /*Path to DataSource*/
+            dtgridview1.DataSource = studentInfo;
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            /*Get inputs*/
+            string name = txtName.Text;
+            string age = txtAge.Text;
+            string city = txtCity.Text;
+            /*Add data to DataTable*/
+            studentInfo.Rows.Add(name, age, city);
+            /*Clear all textboxes data*/
+            txtName.Clear();
+            txtAge.Clear();
+            txtCity.Clear();
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            /*Delete selected row data*/
+            int index = dtgridview1.SelectedRows[0].Index;
+            if (index >= 0) {
+                studentInfo.Rows[index].Delete();
+            }
+        }
+    }
+}
